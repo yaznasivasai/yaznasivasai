35d420c3f07af0d26ffa42bd5098ff928994771b
yaznasivasai <60940642+yaznasivasai@users.noreply.github.com>
2021-09-06 17:03:51 +0530

    Correct

---
+++ main.tf
@@ -0,0 +1,116 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "test_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  tags = {
+    Name = "test_vpc"
+  }
+}
+resource "aws_subnet" "test_pub_subnet" {
+  vpc_id                  = aws_vpc.test_vpc.id
+  cidr_block              = "10.10.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "test_pub_subnet"
+  }
+}
+resource "aws_internet_gateway" "test_igw" {
+  vpc_id = aws_vpc.test_vpc.id
+  tags = {
+    Name : "test_igw"
+  }
+}
+resource "aws_route_table" "test_pub_rt" {
+  vpc_id = aws_vpc.test_vpc.id
+  route = [{
+    carrier_gateway_id         = false
+    cidr_block                 = "0.0.0.0/0"
+    destination_prefix_list_id = false
+    egress_only_gateway_id     = false
+    gateway_id                 = "aws_internet_gateway.test_igw.id"
+    instance_id                = false
+    ipv6_cidr_block            = "::/0"
+    local_gateway_id           = false
+    nat_gateway_id             = false
+    network_interface_id       = false
+    transit_gateway_id         = false
+    vpc_endpoint_id            = false
+    vpc_peering_connection_id  = false
+  }]
+}
+resource "aws_route_table_association" "test_pub_rta" {
+  subnet_id      = aws_subnet.test_pub_subnet.id
+  route_table_id = aws_route_table.test_pub_rt.id
+}
+resource "aws_security_group" "test-sg" {
+  vpc_id = aws_vpc.test_vpc.id
+  name   = "test-sg"
+
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = "ssh"
+      from_port        = "22"
+      to_port          = "443"
+      ipv6_cidr_blocks = ["::/0"]
+      protocol         = "tcp"
+      security_groups  = ["aws_security_group.test_sg.id"]
+      self             = false
+      prefix_list_ids  = [""]
+    }
+  ]
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = "ssh"
+      from_port        = "22"
+      to_port          = "443"
+      ipv6_cidr_blocks = ["::/0"]
+      protocol         = "tcp"
+      security_groups  = ["aws_security_group.test_sg.id"]
+      self             = false
+      prefix_list_ids  = [""]
+    }
+  ]
+
+}
+
+resource "aws_instance" "ec2test" {
+  ami                         = "ami-0b0af3577fe5e3532"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  key_name                    = "test"
+  subnet_id                   = aws_subnet.test_pub_subnet.id
+  security_groups             = ["aws_security_group.test_sg.id"]
+  user_data                   = <<-EOF
+      #!/bin/bash
+      sudo yum-config-manager --disable docker-ce-stable
+      sudo yum update -y
+      sudo yum upgrade -y
+      sudo yum remove docker docker-common docker-selinux docker-engine -y
+      sudo yum install vim epel-release yum-utils device-mapper-persistent-data lvm2 -y
+      sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo -y
+      sudo yum-config-manager --disable docker-ce-stable
+      sudo yum install docker -y
+      sudo systemctl enable docker.service
+      sudo systemctl start docker.service
+      echo "alias docker='sudo docker'" >> ~/.bashrc
+      sudo hostnamectl set-hostname docker
+      sudo groupadd docker
+      sudo usermod -aG docker $USER
+      EOF
+}
+
+data "terraform_remote_state" "myBackend" {
+   backend = "s3"
+   config {
+       bucket = "terraform-state-prod"
+       key = "network/terraform.tfstate"
+       region = "us-east-1"
+   }
+}
